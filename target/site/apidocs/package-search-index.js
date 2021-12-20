6080ae5d7916e8e4cad4061221abc6239b5acef8
SzymonCzajkowski145384 <szymon.z.czajkowski@student.put.poznan.pl>
2021-12-20 11:10:10 +0100

    Adding target directory
    Co-Authored-By: IgorCzudy <77798063+IgorCzudy@users.noreply.github.com>
    Co-Authored-By: bcudzinski <92327682+bcudzinski@users.noreply.github.com>
    Co-Authored-By: mcput <77744014+mcput@users.noreply.github.com>

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pl.put.poznan.buildinginfo.app"},{"l":"pl.put.poznan.buildinginfo.classes"},{"l":"pl.put.poznan.buildinginfo.logic"},{"l":"pl.put.poznan.buildinginfo.rest"}]
